49a0cc9a5869e1b221d75d990530ad71431a9a3b
hzong <zonghuandzzh@126.com>
2019-04-14 19:34:45 +0800

    【提交类型】 初次提交 【内容】spring-demo 【提交人】hzong

---
+++ src/main/java/com/example/hz/demospringboot/entity/hzim/GitEntity.java
@@ -0,0 +1,14 @@
+package com.example.hz.demospringboot.entity.hzim;
+
+import java.io.Serializable;
+
+/**
+ * git测试类
+ * @Classname GitEntity
+ * @Description TODO
+ * @Date 2019-04-14 19:29
+ * @Created by hzong
+ */
+public class GitEntity implements Serializable {
+
+}
