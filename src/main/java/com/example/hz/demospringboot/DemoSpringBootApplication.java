16920cea53982bb3402313406968af9fafc33922
hzong <zonghuandzzh@126.com>
2019-05-27 21:58:57 +0800

    【提交类型】 初次提交 【内容】spring-demo 【提交人】hzong

--- src/main/java/com/example/hz/demospringboot/DemoSpringBootApplication.java
+++ src/main/java/com/example/hz/demospringboot/DemoSpringBootApplication.java
@@ -1,22 +1,23 @@
 package com.example.hz.demospringboot;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+
 /**
  * Mybatis 扫描包
  */
 //@MapperScan("com.example.hz.demospringboot.mapper")
+
 /**
  * 定时类
  */
+@SpringBootApplication
 @EnableScheduling
 @EnableEurekaServer
-public class DemoSpringBootApplication<EnableEurekaServer> {
+public class DemoSpringBootApplication{
 
 
     public static void main(String[] args) {
